9a9dcddb38bd65040729c8684687bf664962a9ea
Abraham Hosu <111804125+Cohdhed@users.noreply.github.com>
2024-06-18 10:45:03 +0100

    Update predict_module.py

--- Deployment/predict_module.py
+++ Deployment/predict_module.py
@@ -59,7 +59,7 @@ def input_page():
     model = st.text_input("Model", ).title()
     y_of_manu = st.selectbox("Year Of Manufacture", ['',2000,2001,2002,2003,2004,2005,2006,2007, 2008,
                                                     2009,2010,2011,2012,2013,2014,2015,2016,2017,
-                                                    2018,2019,2020,2021,2022])
+                                                    2018,2019,2020,2021,2022, 2023])
 
     color = st.text_input("Color", ).title()
     condition = st.selectbox("Condition", ['','Nigerian Used', 'Foreign Used'])
